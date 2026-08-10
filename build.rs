4f6e80938a0860690ae6055518191b8d77f298ef
AloXado320 <david.albujar.s.30@gmail.com>
2026-08-09 23:34:46 -0500

    Update rarezip and fix build on latest rust

--- build.rs
+++ build.rs
@@ -1,5 +1,14 @@
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
-    //link library 
-    println!("cargo:rustc-link-search=rarezip/gzip", );
-    println!("cargo:rustc-link-lib=rarezip", );
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let gzip_dir = PathBuf::from(manifest_dir)
+        .join("rarezip")
+        .join("gzip");
+
+    //link library
+    println!("cargo:rustc-link-search=native={}", gzip_dir.display());
+    println!("cargo:rustc-link-lib=static=rarezip");
+    println!("cargo:rerun-if-changed=build.rs");
 }
